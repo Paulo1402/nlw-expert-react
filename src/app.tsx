4950f6a6a88f5756be5e9040d43be8d05162d1a9
Paulo Benatto <paulobenatto02@gmail.com>
2024-02-11 22:58:19 -0300

    Add Toaster component and update dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -4,33 +4,28 @@ import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="Logo nlw-expert" />
+    <div className="mx-6 max-w-6xl my-12 space-y-6 flex flex-col">
+      <img className="max-w-44" src={logo} alt="Logo nlw-expert" />
 
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500"
+          className="w-full bg-transparent text-1xl font-semibold tracking-tighter outline-none placeholder:text-slate-500 min-[300px]:text-2xl min-[530px]:text-3xl"
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 gap-6 auto-rows-[250px] min-[530px]:grid-cols-3 md">
         <NewNoteCard />
 
         <NoteCard
-          date="Há dois dias"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo necessitatibus, harum, rem adipisci perferendis dolorem maiores doloremque tempore veritatis dolores veniam consequuntur, voluptas inventore asperiores labore fugit consequatur explicabo ducimus!"
-        />
-        <NoteCard
-          date="Há dois dias"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo necessitatibus, harum, rem adipisci perferendis dolorem maiores doloremque tempore veritatis dolores veniam consequuntur, voluptas inventore asperiores labore fugit consequatur explicabo ducimus!"
-        />
-        <NoteCard
-          date="Há quatro dias"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo necessitatibus, harum, rem adipisci perferendis dolorem maiores doloremque tempore veritatis dolores veniam consequuntur, voluptas inventore asperiores labore fugit consequatur explicabo ducimus!"
+          note={{
+            date: new Date(),
+            content:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo necessitatibus, harum, rem adipisci perferendis dolorem maiores doloremque tempore veritatis dolores veniam consequuntur, voluptas inventore asperiores labore fugit consequatur explicabo ducimus!",
+          }}
         />
       </div>
     </div>
